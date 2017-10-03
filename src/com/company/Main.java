2da03b2a55ca6ee14619bb8539ccdb160378015e
petterpinjata <markus_medalen@hotmail.com>
2017-10-03 16:34:51 +0200

    Lagt til ny klasse og flere funksjoner

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,82 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
+    //Tools
+    static Scanner keyboard = new Scanner(System.in);
+
+    //List
+    static List<Movie> movieList = new ArrayList<Movie>();
+
     public static void main(String[] args) {
-	// write your code here
+
+
+        //Movies
+        Movie star_wars = new Movie("Star Wars", true, 0);
+        Movie to_gun = new Movie("Top Gun", true, 0);
+        Movie inception = new Movie("Inception", true, 0);
+        Movie rocky = new Movie("Rocky", true, 0);
+
+        //Add movie into movieList
+        movieList.add(star_wars);
+        movieList.add(to_gun);
+        movieList.add(inception);
+        movieList.add(rocky);
+
+
+        //main loop - allways true
+        while (true){
+
+
+            //Coustomer get list over allowed actions
+            System.out.println("Welcomme to the Movie rental store" + "\n" + "To rent press 'r'");
+            String userCommand = keyboard.nextLine();
+            if(userCommand.equals("r")){
+                userInteraction();
+            }else{
+                System.out.println("Sorry, wrong command!");
+            }
+
+        }
+
     }
+
+    public static void userInteraction(){
+
+        System.out.println("To show all available moives, type 's'" + "\n" + "To return movie, type 'r'" + "\n" + "To search for a movie, type 'f'");
+        String userCommand = keyboard.nextLine();
+        if(userCommand.equals("s")){
+            showAll();
+        }else if(userCommand.equals("r")){
+            returnMovie();
+        }else if(userCommand.equals("s")){
+            searchMoive();
+        }else{
+            System.out.println("Sorry, wrong command!");
+            return;
+        }
+    }
+
+    private static void returnMovie() {
+
+    }
+
+    private static void showAll() {
+        System.out.println("All movies!");
+        for(Movie movie : movieList) {
+            System.out.println(movie.getTitle());
+        }
+        System.out.println("\n");
+        userInteraction();
+    }
+
+    private static void searchMoive() {
+
+    }
+
+
 }
---
+++ src/com/company/Movie.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Movie {
+
+    String title;
+    Boolean inStock;
+    int rentDate;
+
+    public Movie(String title, Boolean inStock, int rentDate) {
+        this.title = title;
+        this.inStock = inStock;
+        this.rentDate = rentDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Boolean getInStock() {
+        return inStock;
+    }
+
+    public int getRentDate() {
+        return rentDate;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setInStock(Boolean inStock) {
+        this.inStock = inStock;
+    }
+
+    public void setRentDate(int rentDate) {
+        this.rentDate = rentDate;
+    }
+}
